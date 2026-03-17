76880a3a6f7e8edfc2fd75abdc0d0678800ad035
kutpiotr <kutpiotr1@gmail.com>
2026-03-17 20:39:31 +0100

    add users search and filtering UI

--- frontend/src/app/features/users/pages/users-page/users-page.component.ts
+++ frontend/src/app/features/users/pages/users-page/users-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 import { UsersListComponent } from '../../components/users-list/users-list.component';
 import { User } from '../../models/user.model';
+import { Role } from '../../models/role.model';
 import { UsersService } from '../../services/users.service';
 
 @Component({
 selector: 'app-users-page',
 standalone: true,
-imports: [CommonModule, RouterLink, UsersListComponent],
+imports: [CommonModule, FormsModule, RouterLink, UsersListComponent],
 templateUrl: './users-page.component.html',
 styleUrl: './users-page.component.css'
 })
@@ -17,18 +19,44 @@ export class UsersPageComponent implements OnInit {
 private usersService = inject(UsersService);
 
 users: User[] = [];
+roles: Role[] = [];
 isLoading = false;
 errorMessage = '';
 
+filters = {
+search: '',
+status: '',
+role_id: ''
+};
+
 ngOnInit(): void {
+this.loadRoles();
 this.loadUsers();
+}
+
+loadRoles(): void {
+this.usersService.getRoles().subscribe({
+next: (roles) => {
+this.roles = roles;
+},
+error: () => {
+this.errorMessage = 'Nie udało się pobrać listy ról.';
+}
+});
 }
 
 loadUsers(): void {
 this.isLoading = true;
 this.errorMessage = '';
 
-this.usersService.getUsers().subscribe({
+const roleId =
+this.filters.role_id !== '' ? Number(this.filters.role_id) : undefined;
+
+this.usersService.getUsers(
+this.filters.search || undefined,
+this.filters.status || undefined,
+roleId
+).subscribe({
 next: (data) => {
 this.users = data;
 this.isLoading = false;
@@ -38,6 +66,20 @@ this.errorMessage = 'Nie udało się pobrać użytkowników.';
 this.isLoading = false;
 }
 });
+}
+
+applyFilters(): void {
+this.loadUsers();
+}
+
+resetFilters(): void {
+this.filters = {
+search: '',
+status: '',
+role_id: ''
+};
+
+this.loadUsers();
 }
 
 deleteUser(userId: number): void {
